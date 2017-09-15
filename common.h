36f6cd1e74479ae5c5b519a68cad50ff2eed5e2b
Maciej Kufel <kufcio97@gmail.com>
2017-09-15 08:46:12 +0200

    Initial changes in farmer

--- common.h
+++ common.h
@@ -15,7 +15,23 @@
  
 
 // TODO: put your definitions of the datastructures here
+// two message queues
+static char    mq_name1[MQ_MAX_MESSAGES];
+static char    mq_name2[MQ_MAX_MESSAGES];
 
+typedef struct
+{
+    // a data structure with 2 members
+    char    startingPoint;
+    char    md5[32];
+} MQ_REQUEST_MESSAGE;
+
+typedef struct
+{
+    // a data structure with 2 members
+    char    hashedValue[32];
+    char    md5[32];
+} MQ_RESPONSE_MESSAGE;
 
 #endif
 
--- farmer.c
+++ farmer.c
@@ -1,14 +1,14 @@
-/* 
+/*
  * Operating Systems [2INCO] Practical Assignment
  * Interprocess Communication
  *
- * STUDENT_NAME_1 (STUDENT_NR_1)
+ * Maciek Kufel
  * STUDENT_NAME_2 (STUDENT_NR_2)
  *
  * Grading:
- * Students who hand in clean code that fully satisfies the minimum requirements will get an 8. 
- * ”Extra” steps can lead to higher marks because we want students to take the initiative. 
- * Extra steps can be, for example, in the form of measurements added to your code, a formal 
+ * Students who hand in clean code that fully satisfies the minimum requirements will get an 8.
+ * ”Extra” steps can lead to higher marks because we want students to take the initiative.
+ * Extra steps can be, for example, in the form of measurements added to your code, a formal
  * analysis of deadlock freeness etc.
  */
 
@@ -19,13 +19,122 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <errno.h>    
+#include <errno.h>
 #include <unistd.h>         // for execlp
 #include <mqueue.h>         // for mq
 
 #include "settings.h"
 #include "common.h"
 
+static void
+message_queue_child (void)
+{
+    mqd_t               mq_fd_request;
+    mqd_t               mq_fd_response;
+    MQ_REQUEST_MESSAGE  req;
+    MQ_RESPONSE_MESSAGE rsp;
+    req.md5[0] = 'q';
+    rsp.hashedValue[0] = 't';
+    req.startingPoint = 'f';
+
+    mq_fd_request = mq_open (mq_name1, O_RDONLY);
+    mq_fd_response = mq_open (mq_name2, O_WRONLY);
+
+    // read the message queue and store it in the request message
+    printf ("                                   child: receiving...\n");
+    mq_receive (mq_fd_request, (char *) &req, sizeof (req), NULL);
+    sleep (3);
+    req.md5[0] = 'q';
+
+    printf ("                                   child: received: %c, %c\n",
+            req.md5[0], req.startingPoint);
+
+    // fill response message
+    // e   contains the length of f[]
+    // f[] contains characters abcd
+    // g[] contains characters ABCDEFGHIJ + \0-terminator (so it is a C-string)
+    rsp.md5[0] = 'z';
+    rsp.hashedValue[0] = 'd';
+
+    sleep (3);
+    // send the response
+    printf ("                                   child: sending...\n");
+    mq_send (mq_fd_response, (char *) &rsp, sizeof (rsp), 0);
+
+    mq_close (mq_fd_response);
+    mq_close (mq_fd_request);
+}
+
+static void
+message_queue_test (void)
+{
+    pid_t               processID;      /* Process ID from fork() */
+    mqd_t               mq_fd_request;
+    mqd_t               mq_fd_response;
+    MQ_REQUEST_MESSAGE req;
+    MQ_RESPONSE_MESSAGE rsp;
+    struct mq_attr      attr;
+
+    mq_fd_request = mq_open (mq_name1, O_RDONLY);
+    mq_fd_response = mq_open (mq_name2, O_WRONLY);
+
+    sprintf (mq_name1, "/mq_request_%s_%d", "Maciek Kufel", getpid());
+    sprintf (mq_name2, "/mq_response_%s_%d", "Ahmed Ahres", getpid());
+
+    attr.mq_maxmsg  = 10;
+    attr.mq_msgsize = sizeof (MQ_REQUEST_MESSAGE);
+    mq_fd_request = mq_open (mq_name1, O_WRONLY | O_CREAT | O_EXCL, 0600, &attr);
+
+    attr.mq_maxmsg  = 10;
+    attr.mq_msgsize = sizeof (MQ_RESPONSE_MESSAGE);
+    mq_fd_response = mq_open (mq_name2, O_RDONLY | O_CREAT | O_EXCL, 0600, &attr);
+
+    processID = fork();
+    if (processID < 0)
+    {
+        perror("fork() failed");
+        exit (1);
+    }
+    else
+    {
+        if (processID == 0)
+        {
+            // child-stuff
+            message_queue_child ();
+            exit (0);
+        }
+        else
+        {
+            // remaining of the parent stuff
+
+            // fill request message
+            req.md5[0] = 'a';
+            req.startingPoint = 'b';
+
+            sleep (3);
+            // send the request
+            printf ("parent: sending...\n");
+            mq_send (mq_fd_request, (char *) &req, sizeof (req), 0);
+
+            sleep (3);
+            // read the result and store it in the response message
+            printf ("parent: receiving...\n");
+            mq_receive (mq_fd_response, (char *) &rsp, sizeof (rsp), NULL);
+
+            sleep (3);
+            printf ("parent: received: %c, %c \n", rsp.hashedValue[0], rsp.md5[0]);
+
+            sleep (1);
+
+            waitpid (processID, NULL, 0);   // wait for the child
+
+            mq_close (mq_fd_response);
+            mq_close (mq_fd_request);
+            mq_unlink (mq_name1);
+            mq_unlink (mq_name2);
+        }
+    }
+}
 
 int main (int argc, char * argv[])
 {
@@ -33,9 +142,10 @@ int main (int argc, char * argv[])
     {
         fprintf (stderr, "%s: invalid arguments\n", argv[0]);
     }
-        
+
+    message_queue_test();
+
     // TODO:
-    //  * create the message queues (see message_queue_test() in interprocess_basic.c)
     //  * create the child processes (see process_test() and message_queue_test())
     //  * do the farming
     //  * wait until the chilren have been stopped (see process_test())
@@ -43,7 +153,6 @@ int main (int argc, char * argv[])
 
     // Important notice: make sure that the names of the message queues contain your
     // student name and the process id (to ensure uniqueness during testing)
-    
+
     return (0);
 }
-
